d3eec9e0f05539a97c443ba3845cd1a997b22183
RuanMoura <ruanmoura13@outlook.com>
2022-05-01 13:01:03 -0300

    container application

---
+++ aplicacao/entrypoints/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+until bundle exec rails db:prepare; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "Postgres is up - executing command"
+
+bundle exec rails server -b 0.0.0.0

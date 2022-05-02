172b1d6d6d88191fef5eddfb5b4634375b1f0673
RuanMoura <ruanmoura13@outlook.com>
2022-05-01 23:08:55 -0300

    add ci

--- aplicacao/entrypoints/docker-entrypoint.sh
+++ aplicacao/entrypoints/docker-entrypoint.sh
@@ -6,11 +6,13 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-until bundle exec rails db:prepare; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+# until bundle exec rails db:prepare; do
+#   >&2 echo "Postgres is unavailable - sleeping"
+#   sleep 1
+# done
   
->&2 echo "Postgres is up - executing command"
+# >&2 echo "Postgres is up - executing command"
+
+bundle exec rails db:prepare
 
 bundle exec rails server -b 0.0.0.0
